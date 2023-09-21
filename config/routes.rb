eecd3fa5807053ffe85c407363d700822bb5536a
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-09-21 11:55:08 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :users, param: :_username
   post '/auth/login', to: 'authentication#login'
-  get '/*a', to: 'application#not_found'
-
   resources :products
+  get '/*a', to: 'application#not_found'
 
 end
