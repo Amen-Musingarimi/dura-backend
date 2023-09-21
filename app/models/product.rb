30a7de6956b70608d087b88194c7c32f6cd55458
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-09-21 18:54:04 +0200

    add cart models associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   has_one_attached :image
+
+  has_many :cart_items
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_one :cart
  
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
