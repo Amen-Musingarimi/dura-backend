8696ef57b9f87aa89015f9ee7fd85a8463bff51f
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-09-29 10:10:37 +0200

    Fix image error by adding image_url

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,50 +3,25 @@ class ProductsController < ApplicationController
 
   # GET /products
   def index
-    @products = Product.all.map do |product|
-      product.attributes.merge(image: encode_image(product.image))
-    end
+    @products = Product.all
     render json: @products
   end
 
   # POST /products
   def create
-    @product = Product.find_by(name: product_params[:name])
-  
-    if @product.nil?
-      # Product does not exist, create a new one
-      @product = Product.new(product_params)
-    else
-      # Product already exists, update it
-      @product.assign_attributes(product_params)
-    end
-  
+    @product = Product.new(product_params)
+    
     if @product.save
-      # Associate the uploaded image with the product
-      @product.image.attach(params[:product][:image])
-  
       render json: @product, status: :created
     else
       render json: @product.errors, status: :unprocessable_entity
     end
   end
-
-
+  
   private
-
-  def encode_image(image)
-    if image.attached?
-      {
-        data: Base64.encode64(image.download),
-        content_type: image.blob.content_type,
-        filename: image.blob.filename.to_s
-      }
-    end
-  end
-
-  # Use strong parameters to allow only the parameters you expect
+  
   def product_params
-    params.require(:product).permit(:name, :english_name, :description, :price, :measurement_unit, :total_units, :image)
+    params.require(:product).permit(:name, :english_name, :description, :price, :measurement_unit, :total_units, :image_url)
   end
 
   def authenticate_request
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
-  has_one_attached :image
 
   has_many :cart_items, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true 
+  validates :image_url, presence: true
   validates :english_name, presence: true, uniqueness: true
   validates_presence_of :description, :price, :measurement_unit, :total_units
 end
